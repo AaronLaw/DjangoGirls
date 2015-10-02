94922622538e805bb1a76be381f858e6785d5efb
Aaron Law <aaronlaw@gmail.com>
2015-10-02 16:51:45 +0800

    Create forms, urls, templates, views for add comment.

--- blog/views.py
+++ blog/views.py
@@ -43,3 +43,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def add_comment_to_post(request, pk):
+    pass
+
